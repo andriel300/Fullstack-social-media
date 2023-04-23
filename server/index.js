b861f6f60c7eafa6d8d1b2f4600a7a90858e52e0
andrieljose <andrielscript@gmail.com>
2023-04-23 11:12:54 -0300

    feat: Add server configurations and file upload endpoint
    This commit adds server configurations such as middleware setup with CORS, helmet, morgan, and body-parser. It also sets up mongoose to connect to a MongoDB database and starts the server listening on the specified port. In addition, it adds an API route for file uploads using multer and GridFS storage.
    
    Changes Made:
    
    - Imported necessary modules for the server
    - Set up middleware using express, cors, helmet, morgan, and body-parser
    - Configured multer for file upload with GridFS storage
    - Added API routes for the server

---
+++ server/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import multer from "multer";
+import GridFsStorage from "multer-gridfs-storage";
+import Grid from "gridfs-stream";
+
+// Load environment variables from .env file
+dotenv.config();
+
+/**
+ * CONFIGURATIONS
+ */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+
+// Middleware setup
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/**
+ * FILE STORAGE CONFIGURATION
+ */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // Start server
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
+// API routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.json({ file: req.file });
+});
