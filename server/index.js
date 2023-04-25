d962f05ec0bfd3740221926bc063afb85c629376
andrieljose <andrielscript@gmail.com>
2023-04-25 19:25:54 -0300

    updated imports

--- server/index.js
+++ server/index.js
@@ -1,48 +1,38 @@
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "./controllers/auth.js";
-import multer from "multer";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
-import GridFsStorage from "multer-gridfs-storage";
-import Grid from "gridfs-stream";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-// Load environment variables from .env file
-dotenv.config();
-
-/**
- * CONFIGURATIONS
- */
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config();
 const app = express();
-
-// Middleware setup
 app.use(express.json());
-app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/**
- * FILE STORAGE CONFIGURATION
- */
+/* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -53,6 +43,15 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
@@ -61,22 +60,10 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    // Start server
-    app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    // ADD THIS DATA ONE TIME //
+    /* ADD DATA ONE TIME */
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
-
-// API routes
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
-
-/* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
